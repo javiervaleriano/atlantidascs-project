f2d48401a5bd66914d834817891ac67443556835
Javier Valeriano <jvalerianoz15@gmail.com>
2024-02-19 00:19:42 -0400

    Cambio de URL de Producción

--- src/envVars.js
+++ src/envVars.js
@@ -1,13 +1,12 @@
 const ENV = window.location.href.includes('localhost') ? 'DEV' : 'PROD';
-const BACKEND_URL = ENV === 'DEV' ? 'http://localhost:8080' : 'https://weak-tan-gosling-belt.cyclic.app';
+const BACKEND_URL = ENV === 'DEV' ? 'http://localhost:8080' : 'https://atlantidascs-backend-production.up.railway.app/';
 const BACKEND_URL_MAIL = `${BACKEND_URL}/api/mail`;
 const CONTACT_EMAIL = 'contactoatlantidascss@gmail.com';
 const QUOTATION_EMAIL = 'servicioatlantidascs@gmail.com';
 
 export {
-  ENV,
   BACKEND_URL,
   BACKEND_URL_MAIL,
-  CONTACT_EMAIL,
-  QUOTATION_EMAIL,
+  CONTACT_EMAIL, ENV, QUOTATION_EMAIL
 };
+
